1aa32aaeaf6543c2e586e4bbbfb56ad0cb9122cd
ckl117 <ckl117@163.com>
2025-03-21 12:43:21 +0000

    add cutlass

--- cute/compile.sh
+++ cute/compile.sh
@@ -1,4 +1,4 @@
 rm -fr build && mkdir build
-nvcc -I/root/paddlejob/workspace/env_run/output/chenkailun/bf16_batch_gemm/PaddleNLP/csrc/third_party/cutlass/include \
+nvcc -I../third_party/cutlass/include \
 -gencode arch=compute_90a,code=sm_90a -O3 -DNDEBUG \
 -o ./build/test test.cu
