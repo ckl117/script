af8e35a77a060ede7540af682e041c49cfa297b6
ckl117 <ckl117@163.com>
2025-03-21 12:45:23 +0000

    submodule update --init

--- cute/compile.sh
+++ cute/compile.sh
@@ -1,4 +1,5 @@
 rm -fr build && mkdir build
+git submodule update --init --recursive
 nvcc -I../third_party/cutlass/include \
 -gencode arch=compute_90a,code=sm_90a -O3 -DNDEBUG \
 -o ./build/test test.cu
